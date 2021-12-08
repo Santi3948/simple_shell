ea86ec152dfe9bd271a6f8435a0cea332e651faa
camia97 <3962@holbertonschool.com>
2021-12-07 17:53:51 -0800

    change betty style

--- buscopath.c
+++ buscopath.c
@@ -1,8 +1,15 @@
 #include "main.h"
+/**
+ * buscopath - concat. /a with path
+ * @path: environment variable path
+ * @a: token in 0 position
+ * Return: concat
+ */
 char *buscopath(char *path, char *a)
 {
 	char *s = str_concat(path, "/");
 	char *x = str_concat(s, a);
+
 	free(s);
-	return(x);
+	return (x);
 }
